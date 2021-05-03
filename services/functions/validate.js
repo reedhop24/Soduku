73c9e4a0abea655a6a08cfa74b3cab6ce18734ae
reedhop <reed@ibqsystems.com>
2021-05-03 09:50:19 -0700

    commiting fully functional app

--- services/functions/validate.js
+++ services/functions/validate.js
@@ -4,7 +4,7 @@ const validateBoard = (board) => {
     const squareCoord = [[0,0], [0,3], [0, 6], [3,0], [3,3], [3, 6], [6,0], [6,3], [6, 6]];
     let k = 0;
     for(let i = 0; i < 9; i++) {
-        if(!col(board, 8, i) || !row(board, i, 8) || !square(board, squareCoord[k][0], squareCoord[k][0])) {
+        if(!col(board, 8, i) || !row(board, i, 8) || !square(board, squareCoord[k][0], squareCoord[k][1])) {
             return false;
         }
         k++;
